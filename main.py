95b5f442119fa9f4732d90b4f4361f973562f303
burak <h.buraksaruhan@gmail.com>
2019-03-25 00:07:38 +0300

    Adding web scraping as image

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler, MessageHandler, Filters, RegexHandler, ConversationHandler
+from selenium import webdriver
+from pyvirtualdisplay import Display
 import requests
 import re
 
@@ -19,16 +22,29 @@ def start(bot, update):
     chat_id = update.message.chat_id
     bot.send_message(chat_id=chat_id, text='Yo hey')
 
-def bop(bot, update):
+def kopek(bot, update):
     url = get_image_url()
     chat_id = update.message.chat_id
     bot.send_photo(chat_id=chat_id, photo=url)
 
+
+def sinema(bot, update):
+    driver = webdriver.Firefox(executable_path='/usr/bin/firefox')
+    driver.get('https://biletinial.com/mekan/ankara-kentpark-prestige')
+    driver.save_screenshot('sample_screenshot_1.png')
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=open("screenshot.jpg", 'rb'))
+
+
+
+
+
 def main():
     updater = Updater('827906624:AAFfx8fc8DiTbj5GQklxlExboxFVYJSe6cs')
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('bop',bop))
+    dp.add_handler(CommandHandler('kopek',kopek))
     dp.add_handler(CommandHandler('start',start))
+    dp.add_handler(CommandHandler('sinema',sinema))
     updater.start_polling()
     updater.idle()
 
